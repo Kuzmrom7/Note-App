66bee233c7ab425524b7cb7a43f221f5f19ea27c
kuzmrom7 <kuzmrom7@gmail.com>
2017-08-17 19:18:22 +0300

    17/08

--- server/app.js
+++ server/app.js
@@ -1,17 +1,23 @@
 import express from 'express'; //import ES6
+import bodyParser from 'body-parser';
+import * as db from './utils/DBUtils.js';
+
+db.setUpConnection();
 
 const app = express();
 
-app.get('/note', (req, res) => {
+app.use(bodyParser.json());
 
+app.get('/note', (req, res) => {
+    db.listNotes().then(data => res.send(data));
 });
 
 app.post('/note', (req, res) => {
-
+    db.createNote(req.body).then(data => res.send(data));
 });
 
 app.delete('/note/:id', (req, res) => {
-
+    db.deleteNote(req.params.id).then(data => res.send(data));
 });
 
 //up and run server
